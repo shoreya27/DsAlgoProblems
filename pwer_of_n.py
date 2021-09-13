3f88356ff66c887c294cc8f0d8704faa769f7d8f
shoreya27 <gupta.shoreya2707@gmail.com>
2021-09-13 23:25:16 +0530

    power of number using recursion

---
+++ pwer_of_n.py
@@ -0,0 +1,21 @@
+def power_of_n(num, power):
+    assert power >= 0 and int(power) == power , "power should be greater than 0"
+    if power == 0:
+        return 1
+    else:
+        return num * power_of_n(num, power-1)
+    
+two_pow_seven = power_of_n(2, 7) #128
+print(two_pow_seven)
+
+three_pow_one = power_of_n(3,1) #3
+print(three_pow_one)
+
+four_pow_zero = power_of_n(4,0) #1
+print(four_pow_zero)
+
+minus_two_pow_three = power_of_n(-2, 3)
+print(minus_two_pow_three)
+
+negative_power = power_of_n(3, -2) #error
+print(negative_power)
