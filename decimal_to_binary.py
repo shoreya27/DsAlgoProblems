379344bc8b6321403f9f7378ed0b779947db20cb
shoreya27 <gupta.shoreya2707@gmail.com>
2021-09-14 23:34:47 +0530

    decimal to binary converter using recursion

---
+++ decimal_to_binary.py
@@ -0,0 +1,14 @@
+def dec_to_bin(n):
+    assert int(n) == n , "number should be a decimal number"
+    if n == 0:
+        return '0' 
+    return dec_to_bin(n//2) + str(n%2)
+
+convert_7 = dec_to_bin(7)
+print(convert_7)
+
+convert_64 = dec_to_bin(64)
+print(convert_64)
+
+convert_1 = dec_to_bin(1)
+print(convert_1)
