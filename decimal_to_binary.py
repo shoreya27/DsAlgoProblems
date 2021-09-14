61427550708bff43fe494447438b26a63553f926
shoreya27 <gupta.shoreya2707@gmail.com>
2021-09-14 23:36:56 +0530

    decimal to binary converter

--- decimal_to_binary.py
+++ decimal_to_binary.py
@@ -1,7 +1,7 @@
 def dec_to_bin(n):
     assert int(n) == n , "number should be a decimal number"
     if n == 0:
-        return '0' 
+        return '' 
     return dec_to_bin(n//2) + str(n%2)
 
 convert_7 = dec_to_bin(7)
@@ -12,3 +12,6 @@ print(convert_64)
 
 convert_1 = dec_to_bin(1)
 print(convert_1)
+
+convert_13 = dec_to_bin(13)
+print(convert_13)
