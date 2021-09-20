04ebfcd91742b69ebd20bde998bf412e35217c6a
shoreya27 <gupta.shoreya2707@gmail.com>
2021-09-20 23:20:24 +0530

    insertion in array

--- array_module.py
+++ array_module.py
@@ -8,3 +8,10 @@ element_arr = array('i', [1,2,3,4,5,6])
 print(element_arr)
 
 #O(1)
+#insert at begining takes O(N)-----> all elements shiftd to right
+element_arr.insert(0, 0)
+print(element_arr)
+
+#inserting at end of array takes O(1) as we kow the location and its empty
+element_arr.insert(7,7)
+print(element_arr)
