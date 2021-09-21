a9dc59720aa670f278d67045bdb7f99874e4214e
shoreya27 <gupta.shoreya2707@gmail.com>
2021-09-21 21:31:52 +0530

    array module

---
+++ array_module.py
@@ -0,0 +1,29 @@
+from array import *
+
+#declaration
+my_arr = array('i')
+print(my_arr)
+
+element_arr = array('i', [1,2,3,4,5,6])
+# print(element_arr)
+
+#O(1)
+#insert at begining takes O(N)-----> all elements shiftd to right
+element_arr.insert(0, 0)
+# print(element_arr)
+
+#inserting at end of array takes O(1) as we kow the location and its empty
+element_arr.insert(7,7)
+# print(element_arr)
+
+'''
+Array Traversal
+Time Complexity is O(n)
+space complexity is O(n)
+'''
+
+def travers_array(arr):
+    for i in arr:
+        print(i)
+
+travers_array(element_arr)
