84324e4aea7aadb9634f6c122972fb1a31dcf72c
shoreya27 <gupta.shoreya2707@gmail.com>
2021-09-22 22:51:58 +0530

    insertion in 2d array

--- two_dimnsional_array.py
+++ two_dimnsional_array.py
@@ -13,3 +13,17 @@ O(n)
 '''
 arr = np.array([[1,2,3],[4,5,6],[7,8,9]])
 print(arr)
+
+'''
+Insertion is done either
+at begining, middle or at the end.
+insert() isused to add [] as a row or column and takes O(mn)
+'''
+newArrr = np.insert(arr,0,[[100,200,300]],0)
+print("Array after inserting a new row is:")
+print(newArrr)
+
+#insrting new array as a column
+okarr = np.insert(arr,0,[[4,5,7]],1)
+print("Array after inserting a new row is:")
+print(okarr)
