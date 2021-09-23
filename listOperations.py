68710f85a39b99ec5bf84e3a676ca392e962f2fb
shoreya27 <gupta.shoreya2707@gmail.com>
2021-09-23 13:01:52 +0530

    list operations access & travesal

---
+++ listOperations.py
@@ -0,0 +1,14 @@
+'''
+List is a Python Built in ds
+Stores an ordered collection of items sequentially
+'''
+
+shoppingList = ["milk", "cheese", "butter", "pencil"]
+
+#Accessing Element same like an array
+print(shoppingList[0])
+print(shoppingList[2])
+
+#list traversal can be done using loop
+for i in shoppingList:
+    print(i, end=" ")
