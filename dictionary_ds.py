d3d1475920bdb445217f014fed983358964e5bdd
shoreya27 <gupta.shoreya2707@gmail.com>
2021-09-29 15:13:39 +0530

    dictionary travrsal

--- dictionary_ds.py
+++ dictionary_ds.py
@@ -9,3 +9,15 @@ my_dict = {"one":"1", "two":2}
 #print --- 2
 print(my_dict["one"])
 print(my_dict["two"])
+
+'''
+Traversal in dictionary
+Time complexity: O(n)
+space complexity: O(1)
+'''
+name = {"name":"shoreya", 
+        "age":25,
+        "degree":"software engineer"
+        }
+for key in name:
+    print(f"key:{key}, value:{name[key]}")
