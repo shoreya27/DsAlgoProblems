65a184708569d32f02ee5b7efa18f84e16d3348b
shoreya27 <gupta.shoreya2707@gmail.com>
2021-10-03 15:48:33 +0530

    dletion in SLL

--- single_linked_list_creation.py
+++ single_linked_list_creation.py
@@ -74,7 +74,38 @@ class SLinkedList:
             index += 1
         print(f"{value} is not present in list")
         return
-
+    
+    def deletion(self, loc):
+        if not self.head:
+            print("list is already empty!")
+            return
+        if loc == 0:#remove from begining
+            if not self.head.next:
+                self.head = None
+                self.tail = None
+            else:
+                self.head = self.head.next
+            return
+        elif loc == -1:#remove from end
+            if not self.head.next:
+                self.head = None
+                self.tail = None
+            else:
+                start = self.head
+                while start.next.next:
+                    start = start.next
+                start.next = None
+                self.tail = start
+            return
+        else:
+            start = self.head
+            pos = 1
+            while pos<loc-1:
+                start = start.next
+                pos+=1
+            start.next = start.next.next
+            return
+    
 singlyList = SLinkedList()
 #create 2 nodes
 node1 = Node(1)
@@ -97,7 +128,24 @@ print()
 # print("***************")
 # newSingly.traversal()
 # print("***************")
-print("........SEARCHING...........")
-newSingly.search(15)
-newSingly.search(41)
-newSingly.search(110)
+# print("........SEARCHING...........")
+# newSingly.search(15)
+# newSingly.search(41)
+# newSingly.search(110)
+
+newSingly.deletion(0)
+newSingly.display()
+print()
+newSingly.deletion(4)
+print("deleting the 4th index")
+newSingly.display()
+print()
+print("deleting last element")
+newSingly.deletion(-1)
+newSingly.display()
+print()
+newSingly.deletion(-1)
+newSingly.display()
+newSingly.deletion(2)
+print()
+newSingly.display()
