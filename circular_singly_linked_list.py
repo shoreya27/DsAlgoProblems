099e53506778e49bac1706fb61b8b47c26f51498
shoreya27 <gupta.shoreya2707@gmail.com>
2021-10-06 12:40:00 +0530

    insertion algorithm

--- circular_singly_linked_list.py
+++ circular_singly_linked_list.py
@@ -3,8 +3,14 @@ Circular singly linked list
 Creation takes O(1)
 and space complexity is O(1)
 '''
-from single_linked_list_creation import Node
+# from single_linked_list_creation import Node
 
+
+class Node:
+    
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next = None
 class CircularLinkedList:
 
     def __init__(self) -> None:
@@ -27,6 +33,37 @@ class CircularLinkedList:
                 break
         print("]")
 
+    def insertion(self, nodeval, loc):
+        #check linkedlist exist
+        if not self.head:
+            print("Linked List does not exist")
+            return
+        node = Node(nodeval)
+        if loc == 0:#at very begining
+            node.next = self.head
+            self.head = node
+            self.tail.next = node
+        elif loc == -1:#at very end
+            node.next = self.tail.next
+            self.tail.next = node
+            self.tail = node
+        else:#find the location and loop 1 node before location where to insert
+            start = self.head
+            count = 1
+            while count < loc:
+                start = start.next
+                count += 1
+            node.next = start.next
+            start.next = node
+        self.display()
+
 circular_ll = CircularLinkedList()
-circular_ll.create_ll(4)
+circular_ll.create_ll(1)
 circular_ll.display()
+#insert 0 at begining
+circular_ll.insertion(0, 0)
+#insert 10 at last
+circular_ll.insertion(10,-1)
+#insert 2-9 in circular_ll
+for i in range(2,10):
+    circular_ll.insertion(i,i)
