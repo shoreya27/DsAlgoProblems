031bd85b23ac743c96c987158284376100060cf1
shoreya27 <gupta.shoreya2707@gmail.com>
2021-10-09 17:27:13 +0530

    insertion in dll

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -30,9 +30,47 @@ class DoublyLinkedList:
         self.tail = node
 
     def display(self):
-        lst = [node.value for node in music]
+        lst = [node.value for node in self]
         print(lst)
+
+    '''
+    Insertion of a node
+    can be done at 3 diff locations
+    In begining , In End, Or at given location
+    '''
+    def insertion(self, nodeVal, loc):
+        if not self.head:
+            print("List does not exist")
+            return
+        node = Node(nodeVal)
+        if loc == 0:#in begining
+            node.next = self.head
+            self.head.prev = node
+            self.head = node
+        elif loc == -1:#at end
+            node.prev = self.tail
+            self.tail.next = node
+            self.tail = node
+        else:
+            start = self.head
+            count = 1
+            while count < loc:
+                start = start.next
+                count += 1
+            node.next = start.next
+            node.prev = start.next.prev
+            start.next.prev = node
+            start.next = node
+        self.display()
+        return
 
 music = DoublyLinkedList()
 music.create(1)
 music.display()
+music.insertion(0,0)
+music.insertion(2,-1)
+music.insertion(5,-1)
+music.insertion(3,3)
+music.insertion(50,-1)
+for i in range(6,50):
+    music.insertion(i,i-1)
