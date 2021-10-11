0bcecedd06b2ff2bf3cf1227f8c75b3563ce4060
shoreya27 <gupta.shoreya2707@gmail.com>
2021-10-11 23:40:01 +0530

    insertion in cdll

--- circular_doubly_linked_list.py
+++ circular_doubly_linked_list.py
@@ -15,7 +15,7 @@ class CircularDoublyLinkedList:
         start = self.head
         while True:
             yield start.value
-            if start == self.head:
+            if start == self.tail:
                 break
             start = start.next
     
@@ -28,6 +28,41 @@ class CircularDoublyLinkedList:
         self.head = self.tail = node
         node.next = node.prev = node
     
+    def insertion(self, nodeval, loc):
+        if not self.head:
+            print("list is empty")
+            return
+        node = Node(nodeval)
+        if loc == 0:#in begining
+            node.next = self.head
+            node.prev = self.tail
+            self.head.prev = node
+            self.head = node
+            self.tail.next = node
+        elif loc == -1:#at the end
+            node.next = self.head
+            node.prev = self.tail
+            self.tail.next = node
+            self.tail = node
+            self.head.prev = node
+        else:
+            start = self.head
+            count = 1
+            while count < loc-1:
+                start = start.next
+                count += 1
+            node.next = start.next
+            node.prev = start
+            start.next.prev = node
+            start.next = node
+        self.display()
+
 songs = CircularDoublyLinkedList()
 songs.creation(1)
 songs.display()
+songs.insertion(0,0)
+songs.insertion(100,-1)
+songs.insertion(2,3)
+for i in range(3,100):
+    songs.insertion(i,i+1)
+    
