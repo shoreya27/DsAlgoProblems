f8c200ffc3c3169f25a21e4866c179f3eec47c1d
shoreya27 <gupta.shoreya2707@gmail.com>
2021-10-21 12:51:29 +0530

    stack using list

--- stack.py
+++ stack.py
@@ -4,5 +4,49 @@ class Stack:
         self.list = []
     
     def __str__(self):
-        self.list.reverse
-        return "\n".join(self.list)
+        return "\n".join(self.list[::-1])
+
+    #isempty
+    def isEmpty(self):
+        if not self.list:
+            return True
+        return False
+
+    #push
+    def push(self, value):
+        self.list.append(value)
+
+    #pop
+    def pop(self):
+        if self.isEmpty():
+            print("Stack is empty")
+            return
+        return self.list.pop()
+    
+    #delete
+    def delete(self):
+        self.list = []
+    
+    #peek
+    def peek(self):
+        if self.isEmpty():
+            print("stack is empty")
+            return
+        return self.list[-1]
+
+books = Stack()
+# print(books.isEmpty())
+books.push("The compound effect")
+books.push("Swami Vivekanand On Himself")
+books.push("The Psychology of money")
+# print(books)
+
+#------------Pop-----------------------#
+print(books.pop())
+print(books.pop())
+# print(books.pop())
+# print(books.pop())
+print("............")
+print(books.peek())
+print("...........")
+print(books)
