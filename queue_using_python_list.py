5aaad09183dc96f40157cdabab0c01a5d031fe00
shoreya27 <gupta.shoreya2707@gmail.com>
2021-10-23 13:59:53 +0530

    queue using python list without any limit

--- queue_using_python_list.py
+++ queue_using_python_list.py
@@ -8,5 +8,39 @@ class Queue:
             return True
         return False
     
+    def enqueue(self, value):
+        self.list.append(value)
+    
+    def __str__(self):
+        return " ".join(map(str,self.list))
+    
+    def dequeue(self):
+        if self.isEmpty():
+            print("Empty Queue")
+            return
+        return self.list.pop(0)
+
+    def peek(self):
+        if self.isEmpty():
+            print("Empty Queue")
+            return
+        return self.list[0]
+
+    def delete(self):
+        self.list = []
+
 orders = Queue()
 print(orders.isEmpty())#true
+orders.enqueue(4)
+orders.enqueue(8)
+orders.enqueue(12)
+print(orders)
+print(f"pop first element:{orders.dequeue()}")
+print("Now Remaining queue is:", orders)
+print(f"pop second element:{orders.dequeue()}")
+# orders.dequeue()
+# orders.dequeue()
+# print(orderss)
+print(orders.peek())
+orders.delete()
+print(orders)
