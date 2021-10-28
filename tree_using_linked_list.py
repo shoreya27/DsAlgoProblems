2f78a86294370227fd39cdca6d58920b5136951e
shoreya27 <gupta.shoreya2707@gmail.com>
2021-10-28 13:56:26 +0530

    pre order traversal in tree

--- tree_using_linked_list.py
+++ tree_using_linked_list.py
@@ -7,4 +7,25 @@ class TreeNode:
 
 newNode = TreeNode("drinks")
 print(newNode)
+left_child = TreeNode("Hot")
+right_child = TreeNode("Cold")
+newNode.left = left_child
+newNode.right = right_child
 
+tea = TreeNode("tea")
+coffee = TreeNode("coffee")
+left_child.left = tea
+left_child.right = coffee
+
+shake = TreeNode("shake")
+right_child.left = shake
+
+
+def preorder_traversal(node):
+    if not node:
+        return
+    print(node.data)
+    preorder_traversal(node.left)
+    preorder_traversal(node.right)
+
+preorder_traversal(newNode)
