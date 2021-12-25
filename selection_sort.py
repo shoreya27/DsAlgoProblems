b86e31262d7b961c3783855e2b674d70ea55c057
shoreya27 <gupta.shoreya2707@gmail.com>
2021-12-25 22:47:13 +0530

    selection_srt

---
+++ selection_sort.py
@@ -0,0 +1,18 @@
+#time complexity is same :O(n)^2
+#selection sort : useful only whn u got a small list of elements
+
+arr = [100,7,1,-5,2,8,9,3,4,6,-7,4,25,14,36,85,45,22]
+
+def selection_sort(arr):
+    print(arr, len(arr))
+    for round in range(len(arr)-1):
+        #find minimum element in each round
+        smallest = arr[round]
+        for ind,element in enumerate(arr[round:]):
+            if element <= smallest:
+                smallest,index = element, ind
+        arr[index+round] = arr[round]
+        arr[round] = smallest
+    print(arr)
+
+selection_sort(arr)
